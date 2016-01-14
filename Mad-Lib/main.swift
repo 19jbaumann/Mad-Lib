8e9eac2920a941aa0f192936d356c467f495024b
Coder <coder@mainlab1.local>
2016-01-14 11:23:30 -0800

    DONE!!!!

--- Mad-Lib/main.swift
+++ Mad-Lib/main.swift
@@ -6,6 +6,24 @@
 //  Copyright © 2016 Coder. All rights reserved.
 //
 
+//This allowes you to use the Input() command
+
+import Foundation
+
+func input() -> String {
+    let keyboard = NSFileHandle.fileHandleWithStandardInput()
+    let inputData = keyboard.availableData
+    let strData = NSString(data: inputData, encoding: NSUTF8StringEncoding)!
+    
+    return strData.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
+}
+
+
+
+
+//This is the menu, telling you how to play
+var menu = "Please select the number of a Mad-Lib!\n1\tPrincess Bride\n2\tSpongebob\n3\tA Letter from Camp\n4\tThe Truth\n5\tStarWars"
+
 
 
 
@@ -14,18 +32,21 @@
 
 func princessBride() {
     print("first name")
-    var firstname = input()
+    let firstname = input()
     print("last name")
-    var lastName = input()
+    let lastName = input()
     print("past tense verb")
-    var pastTenseVerb = input()
+    let pastTenseVerb = input()
     print("Family member")
-    var familymember = input()
+    let familymember = input()
     print("verb")
-    var verb = input()
+    let verb = input()
     
     print("Hello. My name is \(firstname) \(lastName) You \(pastTenseVerb) my \(familymember). Prepare to \(verb)!")
+    
+    menu1()
 }
+
 
 //Second Madlib
 
@@ -56,6 +77,8 @@ func spongebobSquarepants() {
     let animal = input()
     
     print("Captain: Ohhhhh... who lives in a \(fruit) under the \(noun1)? Kids: \(name) Squarepants! Captain: \(adjective1) and \(color) and \(adjective2) is he! Kids: \(name) Squarepants! Captain: If \(adjective3) be somthing you \(noun2)... Kids: \(name) Squarepants! Captain: Then \(verb1) on the \(verb2) and \(noun3) like a \(animal). Kids: \(name) Squarepants!Captain: Ready? Everyone: \(name) Squarepants! \(name) Squarepants! \(name) Squarepants! Captain: \(name)... Squarepants! ahhhahhhahahhaha!")
+
+menu1()
 }
 
 //Third Madlib
@@ -64,65 +87,66 @@ func spongebobSquarepants() {
 func A_Letter_Camp() {
     print("Welcome to Letter From Camp.")
     print("name")
-    var name = input()
+    let name = input()
     print("Relative")
-    var relative = input()
+    let relative = input()
     print("Adjective")
-    var adjective = input()
+    let adjective = input()
     print("Adjective2")
-    var adjective2 = input()
+    let adjective2 = input()
     print("Adjective3")
-    var adjective3 = input()
+    let adjective3 = input()
     print("Adjective4")
-    var adjective4 = input()
+    let adjective4 = input()
     print("Name Of Person In Room")
-    var nameOfPersonInRoom = input()
+    let nameOfPersonInRoom = input()
     print("Adjective5")
-    var adjective5 = input()
+    let adjective5 = input()
     print("verb ending in ED")
-    var verbEndingInED = input()
+    let verbEndingInED = input()
     print("Body part")
-    var bodyPart = input()
+    let bodyPart = input()
     print("verb ending in ING")
-    var verbEndingInIng = input()
+    let verbEndingInIng = input()
     print("noun plural")
-    var nounPlural = input()
+    let nounPlural = input()
     print("noun")
-    var noun = input()
+    let noun = input()
     print("adverb")
-    var adverb = input()
+    let adverb = input()
     print("verb")
-    var verb = input()
+    let verb = input()
     print("verb2")
-    var verb2 = input()
+    let verb2 = input()
     print ("relative2")
-    var relative2 = input()
-    print("person in room")
-    var personInRoom = input()
+    let relative2 = input()
+
    
     
     print("Dear \(name), I am having a(n) \(adjective) time at camp. The counslour is \(adjective2) and the food is \(adjective3). I met \(nameOfPersonInRoom) and we became \(adjective4) friends. Unfortunately, \(nameOfPersonInRoom) is \(adjective5) and I \(verbEndingInED) my \(bodyPart) so we couldn't go \(verbEndingInIng) like everybody else. I need more \(nounPlural) and a \(noun) sharpener, so please \(adverb) \(verb) more when you \(verb2) back. Yor \(relative), \(relative2)")
+
+menu1()
 }
 
 //Fourth Madlib
 
 func TheTruth() {
     print("item")
-    var item = input()
+    let item = input()
     print("creature1")
-    var creature1 = input()
+    let creature1 = input()
     print("item2")
-    var item2 = input()
+    let item2 = input()
     print("complement")
-    var complement = input()
+    let complement = input()
     print("complement2")
-    var complement2 = input()
+    let complement2 = input()
     print("trait")
-    var trait = input()
+    let trait = input()
     print("feeling")
-    var feeling = input()
+    let feeling = input()
     print("feeling2")
-    var feeling2 = input()
+    let feeling2 = input()
     
     print(" As the \(item) and the ashes collide the \(creature1) eyes light up and the truth of the creatures \(item2) comes out. the \(creature1) is just \(complement) and \(complement2), is pure and \(trait), not just \(feeling) but its \(feeling2) and pure. not just a creature but an angel that is trapped in its own dispair")
     
@@ -133,58 +157,46 @@ func TheTruth() {
 
 func starWars() {
     print("adjective")
-    var adjective = input()
+    let adjective = input()
     print("plural noun")
-    var pluralNoun = input()
+    let pluralNoun = input()
     print("noun")
-    var noun = input()
+    let noun = input()
     print("adjective")
-    var adjective1 = input()
+    let adjective1 = input()
     
     print("The Force is a mystical, \(adjective) power. As Jedi Master Obi-Wan Kenobi once said, The Force is an energy field, created by all living \(pluralNoun) that surrounds us, penetrates us, and binds the \(noun) together. Using the power of the force, a jedi can do many \(adjective1) things.")
-}
 
-//This allowes you to use the Input() command
-
-import Foundation
-
-func input() -> String {
-    let keyboard = NSFileHandle.fileHandleWithStandardInput()
-    let inputData = keyboard.availableData
-    let strData = NSString(data: inputData, encoding: NSUTF8StringEncoding)!
     
-    return strData.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
+menu1()
 }
-
-
-//This is the menu, telling you how to play
-var menu = "Please select the number of a Mad-Lib!\n1\tPrincess Bride\n2\tSpongebob\n3\tA Letter from Camp\n4\tThe Truth\n5\tStarWars"
-
 
 //This allowes you to select which madlib to play
+
+func menu1() {
 print("( ͡° ͜ʖ ͡°) Welcome to Mad Lib ( ͡° ͜ʖ ͡°)")
 print("Would you like to play? Yes or No.")
 
 var playCheck = true
 while playCheck {
-    var play = input()
+    let play = input()
     
     if play == "Yes" || play == "yes" {
         print(menu)
-        var select = input()
+        let select = input()
         if select == "1" {
             print(princessBride())
         }
-        if select == "2" {
+        else if select == "2" {
             print(spongebobSquarepants())
         }
-        if select == "3" {
+        else if select == "3" {
             print(A_Letter_Camp())
         }
-        if select == "4" {
+        else if select == "4" {
             print(TheTruth())
         }
-        if select == "5" {
+        else if select == "5" {
             print(starWars())
         }
         
@@ -194,29 +206,30 @@ while playCheck {
         break
     }
 }
-var gh = input()
+let gh = input()
 if gh == "No" || gh == "no" {
     print(menu)
-    var select2 = input()
+    let select2 = input()
     if select2 == "1" {
         print(princessBride())
     }
-    if select2 == "2" {
+    else if select2 == "2" {
         print(spongebobSquarepants())
     }
-    if select2 == "3" {
+    else if select2 == "3" {
         print(A_Letter_Camp())
     }
-    if select2 == "4" {
+    else if select2 == "4" {
         print(TheTruth())
     }
-    if select2 == "5" {
+    else if select2 == "5" {
         print(starWars())
     }
 }
 
 
 
-
+}
+menu1()
 
 
